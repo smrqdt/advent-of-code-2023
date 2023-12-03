296c8ae67f673398db2442f5ec85e6d657c79c22
Simon Marquardt (smrqdt) <simon.marquardt@posteo.de>
2023-12-03 20:33:55 +0100

    modify template to use go:embed for input

--- tmpl.go
+++ tmpl.go
@@ -2,24 +2,20 @@ package main
 
 import (
 	"bufio"
+	_ "embed"
 	"fmt"
-	"os"
+	"strings"
 )
 
-const InputFile = "day00/input"
+//go:embed input
+var input string
 
 func main() {
 	part1()
 }
 
 func part1() {
-	f, err := os.Open(InputFile)
-	if err != nil {
-		panic(fmt.Sprintf("Could not open file %v \n", err))
-	}
-	defer f.Close()
-
-	scanner := bufio.NewScanner(f)
+	scanner := bufio.NewScanner(strings.NewReader(input))
 	for scanner.Scan() {
 		line := scanner.Text()
 		fmt.Printf("%v \n", line)
