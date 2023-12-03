71b13bb0e0f8d9e1b1836f451ecbe9b4fe7cc381
Simon Marquardt (smrqdt) <simon.marquardt@posteo.de>
2023-12-03 01:25:46 +0100

    add day01 part 2

--- day01/day01.go
+++ day01/day01.go
@@ -5,13 +5,16 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"slices"
 	"strconv"
+	"strings"
 )
 
 const InputFile = "day01/input"
 
 func main() {
 	part1()
+	part2()
 }
 
 func part1() {
@@ -41,4 +44,55 @@ func part1() {
 	}
 
 	fmt.Printf("(Part 1): Calibration Sum is %d \n", calibrationSum)
+}
+
+func part2() {
+	f, err := os.Open(InputFile)
+	if err != nil {
+		panic(fmt.Sprintf("Could not open file %v \n", err))
+	}
+	defer f.Close()
+
+	// f := strings.NewReader("two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen")
+
+	scanner := bufio.NewScanner(f)
+
+	calibrationSum := 0
+
+	// Two regexes to solve the problem of overlapping matches (fiveeight must be parsed as 58)
+	// there might be a better solution, but it seems go regexp doesn’t support lookahead (?)
+	re := regexp.MustCompile(`(one|two|three|four|five|six|seven|eight|nine|\d)`)
+	re_last := regexp.MustCompile(`.*(one|two|three|four|five|six|seven|eight|nine|\d)`)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Printf("%s: ", line)
+		for i, word := range []string{} {
+			line = strings.ReplaceAll(line, word, fmt.Sprintf("%d", i+1))
+		}
+		first := re.FindString(line)
+		matches := re_last.FindStringSubmatch(line)
+		last := matches[1]
+		wordToInt := func(word string) int {
+			num64, err := strconv.ParseInt(word, 10, 32)
+			num := int(num64)
+			if err != nil {
+				words := []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+				num = slices.Index(words, word) + 1
+				if num == 0 {
+					panic("Neither word nor digit")
+				}
+			}
+			fmt.Printf("%s→%d, ", word, num)
+			return int(num)
+		}
+		calib := wordToInt(first)*10 + wordToInt(last)
+		fmt.Printf("(%d)\n", calib)
+		calibrationSum += int(calib)
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("(Part 2): Calibration Sum is %d \n", calibrationSum)
 }
